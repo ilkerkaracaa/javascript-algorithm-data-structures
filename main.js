daddd11ac32ff385440c6f118e40c24a17e88a61
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-18 05:12:17 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -124,47 +124,66 @@
 
 // console.log(reverseLinkedList(linkedlist));
 
-class MyArray {
-  constructor() {
-    this.length = 0;
-    this.data = {};
-  }
-  get(index) {
-    return this.data[index];
-  }
-  push(item) {
-    this.data[this.length] = item;
-    this.length++;
-    return this.data;
-  }
-  pop() {
-    const lastItem = this.data[this.length - 1];
-    delete this.data[this.length - 1];
-    this.length--;
-    return lastItem;
-  }
-  deleteAtIndex(index) {
-    const item = this.data[index];
-    this.shiftItems(index);
-    return item;
-  }
-  shiftItems(index) {
-    for (let i = index; i < this.length - 1; i++) {
-      this.data[i] = this.data[i + 1];
-    }
-    console.log(this.data[this.length - 1]);
-    delete this.data[this.length - 1];
-    this.length--;
-  }
-}
+// class MyArray {
+//   constructor() {
+//     this.length = 0;
+//     this.data = {};
+//   }
+//   get(index) {
+//     return this.data[index];
+//   }
+//   push(item) {
+//     this.data[this.length] = item;
+//     this.length++;
+//     return this.data;
+//   }
+//   pop() {
+//     const lastItem = this.data[this.length - 1];
+//     delete this.data[this.length - 1];
+//     this.length--;
+//     return lastItem;
+//   }
+//   deleteAtIndex(index) {
+//     const item = this.data[index];
+//     this.shiftItems(index);
+//     return item;
+//   }
+//   shiftItems(index) {
+//     for (let i = index; i < this.length - 1; i++) {
+//       this.data[i] = this.data[i + 1];
+//     }
+//     console.log(this.data[this.length - 1]);
+//     delete this.data[this.length - 1];
+//     this.length--;
+//   }
+// }
 
-const myArray = new MyArray();
-myArray.push("hi");
-myArray.push("you");
-myArray.push("!");
-myArray.pop();
-myArray.deleteAtIndex(0);
-myArray.push("are");
-myArray.push("nice");
-myArray.shiftItems(0);
-console.log(myArray);
+// const myArray = new MyArray();
+// myArray.push("hi");
+// myArray.push("you");
+// myArray.push("!");
+// myArray.pop();
+// myArray.deleteAtIndex(0);
+// myArray.push("are");
+// myArray.push("nice");
+// myArray.shiftItems(0);
+// console.log(myArray);
+
+//Create a function that reverses a string;
+// "Hello" --> "olleH"
+
+// function reverse(str) {
+//   return String([...str].reverse()).replace(/,/g, "");
+// }
+// console.log(reverse("hi"));
+// console.log(reverse("Homosapiens"));
+
+//Create a function that concatenates two ordered arrays and reorders them
+
+// function mergeSortedArrays(arrOne, arrTwo) {
+//   return arrOne.concat(arrTwo).sort((a, b) => {
+//     return a - b;
+//   });
+// }
+
+// console.log(mergeSortedArrays([1, 2, 3, 1], [3, 4, 5, 6, 1]));
