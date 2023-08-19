757675f224bd352e336feec13617300d30b57de1
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-19 10:37:46 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -219,3 +219,47 @@
 // }
 
 // console.log(mergeSortedArrays([0, 3, 4, 88], [3, 4, 6, 22]));
+
+// class HashTable {
+//   constructor(size) {
+//     this.data = new Array(size);
+//     // this.data = [];
+//   }
+
+//   _hash(key) {
+//     let hash = 0;
+//     for (let i = 0; i < key.length; i++) {
+//       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+//     }
+//     return hash;
+//   }
+
+//   set(key, value) {
+//     let address = this._hash(key);
+//     if (!this.data[address]) {
+//       this.data[address] = [];
+//     }
+//     this.data[address].push([key, value]);
+//     return this.data;
+//   }
+
+//   get(key) {
+//     const address = this._hash(key);
+//     const currentBucket = this.data[address];
+//     if (currentBucket) {
+//       for (let i = 0; i < currentBucket.length; i++) {
+//         if (currentBucket[i][0] === key) {
+//           return currentBucket[i][1];
+//         }
+//       }
+//     }
+//     return undefined;
+//   }
+// }
+
+// const myHashTable = new HashTable(50);
+
+// console.log(myHashTable.set("ilker", 10000));
+// console.log(myHashTable.get("ilker"));
+// console.log(myHashTable.set("apples", 9));
+// console.log(myHashTable.get("apples"));
