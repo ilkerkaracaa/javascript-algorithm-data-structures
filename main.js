c53d17fed322a3775d6b17cfec73a843c0b34d18
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-21 09:21:37 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -365,3 +365,28 @@
 //   }
 //   return undefined;
 // }
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    //Code here
+    this.head.next = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head.next;
+    this.length++;
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+//myLinkedList.append(16);
+console.log(myLinkedList);
