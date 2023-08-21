f146f62f094eb8963f28633ead81a8c02459135a
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-21 11:28:01 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -366,12 +366,62 @@
 //   return undefined;
 // }
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor(value) {
+//     this.head = {
+//       value: value,
+//       next: null,
+//     };
+//     this.tail = this.head;
+//     this.length = 1;
+//   }
+//   append(value) {
+//     //Code here
+//     const newNode = new Node(value);
+//     this.tail.next = newNode;
+//     this.tail = newNode;
+//     this.length++;
+//   }
+//   prepend(value) {
+//     const newNode = new Node(value);
+//     newNode.next = this.head;
+//     this.head = newNode;
+//     this.length++;
+//   }
+//   printList() {
+//     const array = [];
+//     let correntNode = this.head;
+//     while (correntNode !== null) {
+//       array.push(correntNode.value);
+//       correntNode = correntNode.next;
+//     }
+//     return array;
+//   }
+//   insert(index, value) {
+//     let correntNode = this.head;
+//     for (let i = 1; i < index; i++) {
+//       correntNode = correntNode.next;
+//       if (i == index - 1) {
+//       }
+//     }
+//   }
+// }
+
+// let myLinkedList = new LinkedList(10);
+// myLinkedList.append(5);
+// myLinkedList.append(16);
+// myLinkedList.prepend(1);
+// myLinkedList.insert(2, 99);
+// console.log(myLinkedList.printList());
+
+// add a method remove() to the linked list that deletes a node to the specified index.
 
 class LinkedList {
   constructor(value) {
@@ -383,17 +433,67 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    //Code here
-    const newNode = new Node(value);
+    const newNode = {
+      value: value,
+      next: null,
+    };
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
+    return this;
   }
   prepend(value) {
-    const newNode = new Node(value);
+    const newNode = {
+      value: value,
+      next: null,
+    };
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    //Check for proper parameters;
+    if (index >= this.length) {
+      console.log("yes");
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const removedNode = leader.next;
+    leader.next = removedNode.next;
+    this.length--;
   }
 }
 
@@ -401,4 +501,8 @@ let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+console.log(myLinkedList.printList());
+myLinkedList.remove(2);
+console.log(myLinkedList.printList());
